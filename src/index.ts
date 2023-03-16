e081442e4fe343c474ae4e96f6990f4b4363fd28
Flavio <77132531+flaviodev27@users.noreply.github.com>
2023-03-16 12:32:59 -0300

    created db connection

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,21 @@
-let a: string = 'oi'
-console.log(a)
+export class Database {
+    private static database: Database
+    
+    private constructor(
+        private host: string,
+        private user: string,
+        private password: string,
+    ) {}
+    
+    connect(): void {
+        console.log(`Conectado: ${this.host}, ${this.user} ${this.password}`)
+    }
+    static getDatabse(host: string, user:string, password: string): Database {
+        if (Database.database) {
+            return Database.database
+        }
+        Database.database = new Database(host, user, password)
+        return Database.database
+    }
+}
+
