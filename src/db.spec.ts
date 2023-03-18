8cce0519da24127e3117a50a7a79efd8e102accb
Flavio <77132531+flaviodev27@users.noreply.github.com>
2023-03-18 14:40:25 -0300

    added unit tests

--- src/db.spec.ts
+++ src/db.spec.ts
@@ -1,4 +1,16 @@
-it('sei la', () => {
-    const number = 10
-    expect(number).toBe(10)
+import { Database } from "./db"
+
+test('connect() deve imprimir a mensagem de conexão correta', () => {
+    const db = Database.getDatabase('localhost', 'root', 'password')
+    const logSpy = jest.spyOn(console, 'log')
+
+    db.connect()
+
+    expect(logSpy).toHaveBeenCalledWith('Conectado: localhost, root password')
+})
+describe('Database', () => {
+    test('getDatabase() deve retornar um objeto de banco de dados não nulo', () => {
+        const db = Database.getDatabase('', '', '');
+        expect(db).not.toBeNull();
+    })
 })
